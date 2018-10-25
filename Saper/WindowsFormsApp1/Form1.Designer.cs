f280380c0cd2a0eaab0777fae7958d653e0fadf1
xeenlog <43913841+xeenlog@users.noreply.github.com>
2018-10-25 23:59:03 +0200

    Сапер

---
+++ Saper/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace SnakeNooob
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Box = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Box)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Box
+            // 
+            this.Box.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Box.Location = new System.Drawing.Point(12, 12);
+            this.Box.Name = "Box";
+            this.Box.Size = new System.Drawing.Size(1001, 601);
+            this.Box.TabIndex = 0;
+            this.Box.TabStop = false;
+            this.Box.Paint += new System.Windows.Forms.PaintEventHandler(this.Box_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 120;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1024, 631);
+            this.Controls.Add(this.Box);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.Box)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Box;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Saper/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+using System;
+using System.Collections.Generic;
+
+namespace SnakeNooob
+{
+    public partial class Form1 : Form
+    {
+        const int H = 9;
+        const int W = 9;
+        const int S = 40;
+
+        Position mins = new Position(3, 4);
+        List<Position> snakeParts = new List<Position>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            snakeParts.Add(new Position(5, 5));
+        }
+
+        private void Box_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+
+            for (int i = 0; i < 10; i++)
+                graphics.DrawLine(Pens.Green, i * S, 0, i * S, Box.Height);
+
+            for (int i = 0; i < 10; i++)
+                graphics.DrawLine(Pens.Green, 0, i * S, Box.Width, i * S);
+
+            graphics.DrawLine(Pens.DarkRed, S * 0, 0, S * 0, Box.Height);
+            graphics.DrawLine(Pens.DarkRed, 0, S * 0, Box.Width, S * 0);
+            graphics.DrawLine(Pens.DarkRed, S * 9, 0, S * 9, Box.Height);
+            graphics.DrawLine(Pens.DarkRed, 0, 9 * S, Box.Width, 9 * S);
+
+
+            Random rand = new Random();
+            mins.X = rand.Next(0, W);
+            mins.Y = rand.Next(0, H);
+
+            graphics.FillEllipse(Brushes.Firebrick, mins.X * S, mins.Y * S, S, S);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Box.Refresh();
+        }
+
+        private bool IsHeadOnBody()
+        {
+            for (int i = 0; i < snakeParts.Count; i++)
+            {
+                if (snakeParts[2] == snakeParts[i])
+                    return true;
+            }
+            
+            return false;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+ 
+        }
+    }
+}
